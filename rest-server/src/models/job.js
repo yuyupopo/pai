775d09609b9addb3ac63999c47aa3041394bd4b3
Qixiang Cheng <Qixiang.Cheng@microsoft.com>
2018-08-22 18:39:20 +0800

    lint

--- rest-server/src/models/job.js
+++ rest-server/src/models/job.js
@@ -353,7 +353,7 @@ class Job {
           'taskData': data.taskRoles[idx],
           'jobData': data,
           'inspectPidFormat': '{{.State.Pid}}',
-          'inspectOOMKilledFormat': '{{.State.OOMKilled}}'
+          'inspectOOMKilledFormat': '{{.State.OOMKilled}}',
         });
     return yarnContainerScript;
   }
