9bcb44a26293c1b3e042c59c63a5e7bf0cc073d3
YitongFeng <imfengyitong@hotmail.com>
2018-08-28 09:07:40 +0000

    refine

--- pai-management/test.sh
+++ pai-management/test.sh
@@ -2,14 +2,12 @@
 
 set -e
 
-i=0
-
-while ((i<100)); do
-  ./paictl.py cluster k8s-bootup -p ~/pai-config/
+for((i=0;i<100;i++)); do
+  ./paictl.py cluster k8s-bootup -p ./pai-config/
   echo "*********************************** k8s bootup succesfully! ********************************"
   sleep 10
-  ./paictl.py cluster k8s-clean -p ~/pai-config/ -f
+  ./paictl.py cluster k8s-clean -p ./pai-config/ -f
   echo "finished once"
-  let i++
+  echo $i
 done
 
