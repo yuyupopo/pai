873b1e2b94b9fdf16c3c1e436b69283481cc8205
YitongFeng <imfengyitong@hotmail.com>
2018-08-28 08:52:46 +0000

    add config file

--- pai-management/test.sh
+++ pai-management/test.sh
@@ -6,8 +6,10 @@ i=0
 
 while ((i<100)); do
   ./paictl.py cluster k8s-bootup -p ~/pai-config/
+  echo "*********************************** k8s bootup succesfully! ********************************"
+  sleep 10
   ./paictl.py cluster k8s-clean -p ~/pai-config/ -f
-   echo "finished once"
-   let i++
+  echo "finished once"
+  let i++
 done
 
